f415315eac999e142eb3ce9e0ee8b3dc6bee7572
Sergey <ser_8904@mail.ru>
2022-03-26 15:26:34 +0300

    remove fs.createWriteStream

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@ import axios from 'axios';
 import path from 'path';
 import fs from 'fs/promises';
 import {
-  getFileName, getDirName, getNameFromUrl, getLocalLinks, editHtml
+  getFileName, getDirName, getNameFromUrl, getLocalLinks, editHtml,
 } from './utilities.js';
 
 // Здесь будут функции с побочными эффектами
 const createDir = (dirpath) => fs.mkdir(dirpath, { recursive: true });
-const loadAsset = (url, outputPath) => axios.get(url, { responseType: 'stream' })
-  .then((response) => response.data.pipe(fs.createWriteStream(outputPath)));
 
 // Основная функция прнимает ссылку в виде строки
 // И путь до папки куда складывать результат
-export default (url, ouputDir = process.cwd()) => {
+export default (url, outputDir = process.cwd()) => {
   const { origin } = new URL(url);
   const fileName = getFileName(getNameFromUrl(url));
-  const filePath = path.join(ouputDir, fileName);
+  const filePath = path.join(outputDir, fileName);
   const assetsDirName = getDirName(getNameFromUrl(url));
-  const assetsDirPath = path.join(ouputDir, assetsDirName);
+  const assetsDirPath = path.join(outputDir, assetsDirName);
   return axios.get(url)
-    .then(({ data }) => data)
-    .then((content) => {
-      const localLinks = getLocalLinks(content, origin);
-    
+    .then(({ data }) => {
+      const localLinks = getLocalLinks(data, origin);
+      return localLinks.map((link) => axios.get(link, { responseType: 'arraybuffer' })
+        .then((res) => fs.writeFile(filePath, res.data)));
+    });
+};
